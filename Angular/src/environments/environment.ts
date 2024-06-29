d0dbe80a4a9b00d53ff211d05369f00006861efa
MOSHE9647 <isaacmhp2001@gmail.com>
2024-06-29 14:29:36 -0600

    ...

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const Environment = {
     production: false,
-    apiUrl: 'http://25.7.147.209:',
+    apiUrl: 'http://localhost:',
     apiJasonUrl: 'http://25.9.82.108:',
     djangoPort: '8000',
     reactPort: '3000',
